4abac5f55a4b12ec8b8efc97447ac2eaaa577547
Oluchi <okparabon@gmail.com>
2023-05-09 12:03:31 +0100

    Update schema.sql
    Create Table with name animals

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    id INTEGER PRIMARY KEY,
+    name STRING NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INTEGER,
     neutered BOOLEAN,
