38608750b94afe9f1e46d5a538aba3560be01a9d
Luchy-B <okparabon@gmail.com>
2023-05-09 16:58:50 +0100

    revert changes

--- schema.sql
+++ schema.sql
@@ -1,10 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER PRIMARY KEY,
-    name STRING NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INTEGER,
-    neutered BOOLEAN,
-    weight_kg DECIMAL(5,2) NOT NULL
+    name varchar(100)
 );
